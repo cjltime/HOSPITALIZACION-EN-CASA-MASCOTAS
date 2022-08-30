fac22294667149e33e5153da41cf2a925e2c2c3b
juan <larrota.juancamilo@gmail.com>
2022-08-30 10:44:42 -0500

    Configuracion basica de la app consola

---
+++ PetCare.App/PetCare.App.Consola/Program.cs
@@ -0,0 +1,17 @@
+﻿using PetCare.App.Dominio;
+using PetCare.App.Persistencia;
+
+
+namespace PetCare.App.Consola
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World Entity framework! ");
+        }
+    }
+}
+
+// See https://aka.ms/new-console-template for more information
+

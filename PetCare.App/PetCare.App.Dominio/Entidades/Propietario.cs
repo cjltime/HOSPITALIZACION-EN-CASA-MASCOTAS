eeb5192302f2d106e0dea2d4c24fc58a6bed03da
juan <larrota.juancamilo@gmail.com>
2022-08-30 10:32:36 -0500

    Configuracion del AppContext

--- PetCare.App/PetCare.App.Dominio/Entidades/Propietario.cs
+++ PetCare.App/PetCare.App.Dominio/Entidades/Propietario.cs
@@ -1,7 +1,7 @@
 using System;
 namespace PetCare.App.Dominio
 {
-    public class Propietario
+    public class Propietario: Persona
     {
         public string Direccion{get;set;}
         public string  Edad{get;set;}
---
+++ PetCare.App/PetCare.App.Persistencia/AppContext/AppContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using PetCare.App.Dominio;
+
+namespace PetCare.App.Persistencia
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<HistoriaClinica> HistoriasClinicas { get; set; }
+        public DbSet<Mascota> Mascotas { get; set; }
+        public DbSet<Persona> Personas { get; set; }
+        public DbSet<Propietario> Propietarios { get; set; }
+        public DbSet<SignoVital> SignosVitales { get; set; }
+        public DbSet<Veterinario> Veterinarios { get; set; }
+        public DbSet<Visita> Visitas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+               .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog =PetCareData");
+            }
+        }
+    }
+}
